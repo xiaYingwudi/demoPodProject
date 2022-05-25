be1e5271c73fa9077247ead44b61d8dc83b5caf0
2442285459@qq.com <2442285459@qq.com>
2022-05-25 22:19:16 +0800

    no message

---
+++ demoPodProject/Classes/XYModel.h
@@ -0,0 +1,19 @@
+//
+//  XYModel.h
+//  demoPodProject
+//
+//  Created by 夏寅 on 2022/5/25.
+//  Copyright © 2022 夏寅. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XYModel : NSObject
+
+static int 2 ;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ demoPodProject/Classes/XYModel.m
@@ -0,0 +1,13 @@
+//
+//  XYModel.m
+//  demoPodProject
+//
+//  Created by 夏寅 on 2022/5/25.
+//  Copyright © 2022 夏寅. All rights reserved.
+//
+
+#import "XYModel.h"
+
+@implementation XYModel
+
+@end
